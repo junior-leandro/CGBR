ac3b2f392826a2933a1624cd5f0ab602c7159276
Junior Oliveira Leandro <contato_jr@outlook.com>
2024-02-27 11:22:31 -0300

    confetti on the result

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 
-function generateNumber(){
 
-    const min = Math.ceil(document.querySelector (".input-min").value)
-    const max = Math.floor(document.querySelector (".input-max").value)
-    
+function generateNumber() {
+
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
-    
+
     document.querySelector("#result > span").textContent = result;
-    
-    }
+
+    var confettiSettings = { target: 'my-canvas' };
+    var confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+
+}
