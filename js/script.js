84ce85dfadb0f638c8372e83e943c883fd7e07c5
Junior Oliveira Leandro <contato_jr@outlook.com>
2024-02-28 10:14:33 -0300

    setTimeout and initial result hiding

--- js/script.js
+++ js/script.js
@@ -2,15 +2,30 @@
 
 function generateNumber() {
 
-    const min = Math.ceil(document.querySelector(".input-min").value)
-    const max = Math.floor(document.querySelector(".input-max").value)
+    const min = Math.ceil(document.querySelector(".input-min").value);
+    const max = Math.floor(document.querySelector(".input-max").value);
+    const resultName = document.querySelector(".result-name");
+    const resultNumber = document.querySelector(".result-number");
+
 
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     document.querySelector("#result > span").textContent = result;
 
-    var confettiSettings = { target: 'my-canvas' };
-    var confetti = new ConfettiGenerator(confettiSettings);
-    confetti.render();
+
+    setTimeout(function () {
+        var confettiSettings = { target: 'my-canvas' };
+        var confetti = new ConfettiGenerator(confettiSettings);
+        confetti.render();
+
+        resultName.classList.remove("ocult");
+        resultNumber.classList.remove("ocult");
+
+        
+
+    }, 2200);
+
+    
+
 
 }
