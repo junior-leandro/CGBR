a1e18624178cfac8838dcf2651f5e9c0bdca0c5f
Junior Oliveira Leandro <contato_jr@outlook.com>
2024-02-29 08:44:26 -0300

    playing sounds and hiding and showing texts

--- js/script.js
+++ js/script.js
@@ -6,12 +6,22 @@ function generateNumber() {
     const max = Math.floor(document.querySelector(".input-max").value);
     const resultName = document.querySelector(".result-name");
     const resultNumber = document.querySelector(".result-number");
+    const canva = document.querySelector("#my-canvas");
 
+    canva.classList.add("ocult");
 
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     document.querySelector("#result > span").textContent = result;
 
+    const audio = new Audio('../audio/tambores.mp3');
+    audio.addEventListener('canplaythrough', function () {
+        audio.play();
+    });
+    
+    resultName.classList.add("ocult");
+    resultNumber.classList.add("ocult");
+    
 
     setTimeout(function () {
         var confettiSettings = { target: 'my-canvas' };
@@ -20,12 +30,14 @@ function generateNumber() {
 
         resultName.classList.remove("ocult");
         resultNumber.classList.remove("ocult");
+        canva.classList.remove("ocult");
 
-        
-
+        setTimeout (function(){
+            const audio2 = new Audio('../audio/baby-shark.mp3');
+            audio2.addEventListener('canplaythrough', function () {
+                audio2.play();
+            });
+        }, 1000);
     }, 2200);
-
-    
-
 
 }
